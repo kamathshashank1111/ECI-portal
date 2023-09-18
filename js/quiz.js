cfb8c12ea6ccc9676102d4a035c28fb1bf4ae401
kamathshashank1111 <kamathshashank1111@gmail.com>
2023-09-18 19:14:08 +0530

    quiz added

---
+++ js/quiz.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who was the first Election Commissioner of India?",
+    answer: "Sukumar Sen",
+    options: [
+      "Sukumar Sen",
+      "VS Ramadevi",
+      "Shankar Dayal Sharma ",
+      "Rajendra Prasad"
+    ]
+  },
+  
+    {
+    numb: 2,
+    question: "Which article of the Indian Constitution states that there would be an Election Commission to control, direct elections in India?",
+    answer: "Article 324",
+    options: [
+      "Article 344",
+      "Article 324",
+      "Article 333",
+      "Article 370"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where is the Secretariat of the Election Commission located?",
+    answer: "New Delhi",
+    options: [
+      "New Delhi",
+      "Mumbai",
+      "Gurgaon",
+      "Chandigarh"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who appoints the Election Commissioners?",
+    answer: "President of India",
+    options: [
+      "Chief Election Commissioner",
+      "President of India",
+      "Prime Minister of India",
+      "Parliament of India"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the tenure of the Election Commissioner of India?",
+    answer: "6 years or 65 years of age,  whichever earlier",
+    options: [
+      "5 years or 65 years of age, whichever earlier",
+      "6 years or 65 years of age,  whichever earlier",
+      "4 years or 63 years,  whichever earlier",
+      "5 years or 62 years,  whichever earlier"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
